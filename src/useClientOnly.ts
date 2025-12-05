f1d2afccd8901438df86e235275e35dc883bae29
axtk <axtkx@inbox.ru>
2025-12-05 12:50:57 +0300

    lint

--- src/useClientOnly.ts
+++ src/useClientOnly.ts
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 /**
  * Returns `true` if the component is rendered on the client,
  * and `false` otherwise.
- * 
+ *
  * @example
  * ```js
  * let isClient = useClientOnly();
@@ -11,7 +11,7 @@ import { useEffect, useState } from "react";
  */
 export function useClientOnly() {
   let [isClient, setClient] = useState(false);
-  
+
   useEffect(() => {
     setClient(true);
   }, []);
